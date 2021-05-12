3fb97651c9e1d561f1f6c108fd7d043e3f3df53d
Constantin <cocoh2012@gmail.com>
2021-05-12 12:05:06 +0200

    V1 upload

---
+++ Assets/scripts/ennemiMoving.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ennemiMoving : MonoBehaviour
+{
+
+    public float speed;
+    public float maxSpeed;
+
+    private int up = 1;
+    private int left = 1;
+
+    private int speedloop = 10;
+    private int coutner = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.coutner++;
+
+        if (this.coutner >= this.speedloop )
+        {
+            if (this.speed <= maxSpeed)
+            {
+                this.speed += Time.deltaTime;
+            }
+
+            this.coutner = 0;
+        }
+
+        this.Move();
+        
+    }
+
+    void Move()
+    {
+        this.transform.position = this.transform.position +  new Vector3(((this.speed * this.left) * 0.01F), ((this.speed * this.up) * 0.01F), 0);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        string tag = col.gameObject.tag;
+        Debug.Log(tag);
+        if (tag == "side")
+            this.left *= -1;
+
+        if (tag == "updown")
+            this.up *= -1;
+    }
+
+
+}
---
+++ Assets/scripts/playerMoving.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerMoving : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool up = false;
+        bool down = false;
+        bool left = false;
+        bool right = false;
+
+        if (Input.GetKey(KeyCode.W))
+            up = true;
+
+        if (Input.GetKey(KeyCode.A))
+            left = true;
+
+        if (Input.GetKey(KeyCode.S))
+            down = true;
+
+        if (Input.GetKey(KeyCode.D))
+            right = true;
+
+        this.Move(up, down, left, right);
+    }
+
+    private void Move(bool u, bool d, bool l, bool r)
+    {
+        float v = 0;
+        float h = 0;
+
+        if (u)    
+            h++;
+        if (d)
+            h--;
+
+        if (l)
+            v--;
+        if (r)
+            v++;
+
+        this.transform.position = this.transform.position + new Vector3((this.speed * v) * 0.01F, (this.speed * h) * 0.01F, 0);
+    }
+
+    void GameOver()
+    {
+        SceneManager.LoadScene("title");
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        string tag = col.gameObject.tag;
+        Debug.Log(tag);
+        if (tag == "twitter")
+            this.GameOver();
+    }
+}
